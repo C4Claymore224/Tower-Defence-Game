ae9c95e7d6514c695d62c3ace984995c06a61c8e
Lucian Rectanus <lucian@rectanus.net>
2025-12-12 00:28:28 -0500

    Branch Creation for just the godot project

--- app/data/init.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE scores (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        rank INTEGER NOT NULL,
-                        player TEXT NOT NULL,
-                        score INTEGER NOT NULL
-);
